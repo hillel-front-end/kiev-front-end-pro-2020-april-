e81374b0e22b41d5592a0f5edb281dfd9246f3db
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2020-05-30 19:42:49 +0300

    l-11_OOP

---
+++ lections/lection_11_OOP/materials/main.js
@@ -0,0 +1,98 @@
+
+
+// var obj = {name: 'valera'};
+
+// function foo() {
+//  console.log('function foo with this == ', this.name)
+// }
+
+
+// foo.call(obj2);
+
+
+// var foo = foo.bind(obj2);
+
+// console.log(foo, 'foo');
+
+// foo();
+// foo();
+
+
+
+// setTimeout(foo.bind(obj), 3000);
+
+// function setTimeout(callback, time) {
+//     callback();
+// }
+
+// setTimeout(foo.bind(obj), 3000);
+
+// ------------------ OOP ---------------
+
+// var obj = new Object(); //{}
+
+// console.log(obj, 'obj');
+// var mass = new Array();
+
+
+function Animal(name) {
+    // console.log(this) // this == dog
+    this.name = name;
+    this.run = function() {
+        console.log('run');
+    }
+}
+
+
+
+var dog = new Animal('sharik'); // 1) create new object
+var cat = new Animal('murzik'); 
+var aaa = new Animal('hz'); 
+// console.log(dog, 'dog');
+
+// Prototype
+
+
+// console.dir(Animal.prototype, 'Animal'); // {}
+// console.log(dog.__proto__, 'dog proto');
+
+// console.log(Animal.prototype == dog.__proto__);
+
+
+function HTMLElement(tagName) {
+    this.tagName = tagName;
+
+    if(tagName == 'a') {
+        this.redirect = function() {
+            console.log('redirect');
+        }
+    } else if(tagName == 'img') {
+        this.showPicture = function() {
+            console.log(' show picture');
+        }
+    }
+
+}
+
+console.dir(HTMLElement, 'HTMLElement');
+
+HTMLElement.prototype.render = function() {
+    console.log('rendering -->>', this.tagName);
+}
+
+var a = new HTMLElement('a');
+var img = new HTMLElement('img');
+
+
+
+// a.redirect();
+// img.showPicture();
+
+console.log(a, 'a');
+console.log(img, 'img');
+
+a.render();
+img.render();
+
+
+func = new Function();
