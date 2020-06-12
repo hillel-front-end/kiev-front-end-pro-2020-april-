cc574056dfa6821229a5d7caf66cab296b3f5c45
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2020-06-12 18:59:12 +0300

    l-14

---
+++ lections/lection_14_DOM_events/materials/main.js
@@ -0,0 +1,25 @@
+console.log('Lection_1');
+window.onload = function() {
+/*
+Dom navigation
+*/
+
+/*events [ 
+    click, 
+    mousemove, 
+    mouseleave, 
+    onmousedown,  
+    onmouseup,
+    onmouseover, 
+    onmouseout,
+    onmousemove,
+    contextmenu 
+], 
+
+*/
+
+}
+
+
+
+
